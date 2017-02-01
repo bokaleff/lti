37afab378a2ec2545af3b0dc394f2db4fbc62193
Yago <yago.ribeiro@univesp.br>
2017-02-01 10:30:09 -0200

    form post

--- app.rb
+++ app.rb
@@ -1,12 +1,22 @@
 require 'sinatra'
 require 'ims/lti'
 
-get '/test' do
-	"ola"
+configure do
+  # Allows Canvas to embed the LTI like a frame
+  set :protection, :except => :frame_options
 end
 
-get '/' do
-  redirect "https://apps.univesp.br/rea-deficiencia"
+get '/test' do
+  "ola"
+end
+
+post '/' do
+  return "<form method="post" action="https://apps.univesp.br/rea-deficiencia">
+    		<input type="submit" style="display:none"></input>
+		  </form>
+		  <script>
+    		window.location.href = "https://apps.univesp.br/rea-deficiencia";
+       	  </script>"
   #params[:custom_canvas_user_login_id]
 end
 
