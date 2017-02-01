8316f665e755f3cb60fbfd166091ffc933b50d8b
Evandro Almeida <evandrodevops@gmail.com>
2017-02-01 14:18:52 -0200

    Encoding and sending the user id to the target app

--- app.rb
+++ app.rb
@@ -1,18 +1,16 @@
 require 'sinatra'
 require 'ims/lti'
+require 'base64'
 
 configure do
   # Allows Canvas to embed the LTI like a frame
   set :protection, :except => :frame_options
 end
 
-get '/test' do
-  "ola"
-end
-
 post '/' do
-  return '<form method="post" action="https://apps.univesp.br/rea-deficiencia" target="_blank" style="display:none">
-    <input type="hidden" name="ra" value="' + params[:custom_canvas_user_login_id] + '">
+  encoded_ra = Base64.encode64(params[:custom_canvas_user_login_id].strip)
+  return '<form method="get" action="https://apps.univesp.br/rea-deficiencia" target="_blank" style="display:none">
+    <input type="hidden" name="r" value="' + encoded_ra + '">
     <input type="submit">
     </form>
     <script>
